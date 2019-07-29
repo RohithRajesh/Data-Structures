2454cce926fd84c8a70e82905d4b73f0b77b7df8
RohithRajesh <rohith18182@iiitd.ac.in>
2019-07-30 00:42:17 +0530

    Linkedlist

---
+++ ds.java
@@ -0,0 +1,127 @@
+public class ds{
+	public static void main(String[] args){
+		LinkedList ll= new LinkedList();
+		ll.append(1);
+		ll.append(2);
+		ll.append(3);
+		ll.append(4);
+		ll.append(5);
+		ll.delete(3);
+		ll.display();
+
+	
+	}
+
+	public static class Node{
+		int data;
+		Node next;
+		public Node(){
+			data=0;
+			next=null;
+		}
+	}
+	public static class LinkedList{
+		Node head=new Node();
+		int size;
+
+		public LinkedList(){
+			size=0;
+			head.next=null;
+		}
+		public void append(int value){
+			Node n1=new Node();
+			n1.data=value;
+			if(size==0){
+				head=n1;
+				size++;
+			}
+			else{
+			Node nptr=head;
+			while(nptr.next!=null){
+				nptr=nptr.next;
+			}
+			nptr.next=n1;
+			size++;
+		}
+		}
+		// position follows 0 indexing.
+		public void insert(int position,int value){
+			Node n1=new Node();
+			n1.data=value;
+			if(position==0){
+				if(size==0){
+					head=n1;
+					size++;
+				}
+				else{
+					n1.next=head;
+					head=n1;
+					size++;
+			
+				}
+			}
+			else{
+				Node nptr=head;
+				int count=0;
+				while(count!=position-1){
+					nptr=nptr.next;
+					count++;
+				}
+				n1.next=nptr.next;
+				nptr.next=n1;
+				size++;
+		}
+		}
+		public void delete(int position){
+			if(position==0){
+				head=head.next;
+			}
+			else{
+				Node nptr=head;
+				int count=0;
+				while(count!=position-1){
+					nptr=nptr.next;
+					count++;
+				}
+				nptr.next=nptr.next.next;
+			}
+			size--;
+		}
+		public void display(){
+			Node nptr=head;
+			while(nptr!=null){
+				System.out.println(nptr.data);
+				nptr=nptr.next;
+			}
+		}	
+
+	}
+	public static class Stack(){
+		int[] s=new int[10000000];
+		int front;
+		public Stack(){
+			front=0;
+		} 
+		public void push(int x){
+			
+
+		}
+
+
+
+
+
+	}
+		
+
+
+
+
+
+
+
+
+
+		}
+
+
